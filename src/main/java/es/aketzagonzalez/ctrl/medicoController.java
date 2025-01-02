409028d5cd1219d3386f67651f68c42305ef854c
FormedNumber95 <aketza200510@gmail.com>
2025-01-02 16:11:58 +0100

    Initial commit

---
+++ src/main/java/es/aketzagonzalez/ctrl/medicoController.java
@@ -0,0 +1,56 @@
+package es.aketzagonzalez.ctrl;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class medicoController {
+
+    @FXML
+    private Button btnGenerar;
+
+    @FXML
+    private Button btnLimpiar;
+
+    @FXML
+    private Button btnSalir;
+
+    @FXML
+    private TextField txtCodMedico;
+
+    @FXML
+    private TextField txtDireccionPaciente;
+
+    @FXML
+    private TextField txtEspecialidadMedico;
+
+    @FXML
+    private TextField txtNombreMedico;
+
+    @FXML
+    private TextField txtNombrePaciente;
+
+    @FXML
+    private TextField txtNumPaciente;
+
+    @FXML
+    private TextArea txtTratamientos;
+
+    @FXML
+    void accionGenerar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionLimpiar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accionSalir(ActionEvent event) {
+
+    }
+
+}
